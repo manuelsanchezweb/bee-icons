07c3fd84861c810ea4b86fcf1721d5264ec14150
manuelsanchez2 <manusansan22@gmail.com>
2023-04-01 10:31:14 +0200

    🆕 feat: Add initial project

---
+++ README.md
@@ -0,0 +1,3 @@
+# bee-icons
+
+To edit or add new icons, just go to <code>src/data/icons.ts</code>
---
+++ src/data/icons.ts
@@ -0,0 +1,46 @@
+////// xs, sm, md, lg, xl are the different sizes of the icon
+
+export const icons = [
+  {
+    id: 1,
+    name: "search",
+    mainCategory: "basics",
+    tags: ["search, lupe, zoom"],
+    icon: {
+      xs: "<svg>...</svg>",
+      sm: "",
+      md: "",
+      lg: "",
+      xl: "",
+    },
+  },
+  {
+    id: 2,
+    name: "pig",
+    mainCategory: "animals",
+    tags: ["pig, piggy, piglet"],
+    icon: {
+      xs: `<svg>...</svg>`,
+      sm: ``,
+      md: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+      <g clip-path="url(#clip0_12_1054)">
+      <circle cx="7.48816" cy="8" r="1" fill="black"/>
+      <ellipse cx="12.7997" cy="14.5" rx="0.799729" ry="1.5" fill="black"/>
+      <ellipse cx="10.6521" cy="14.5" rx="0.799729" ry="1.5" fill="black"/>
+      <path d="M5.79842 4.27474L3.41891 6.40792C2.83582 6.93066 1.90535 6.61067 1.7671 5.83987L1.17223 2.5232C1.03376 1.75117 1.79745 1.12753 2.52622 1.4175L5.50061 2.60099C6.19034 2.87543 6.35115 3.77922 5.79842 4.27474Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M18.3089 4.1581L20.6884 6.29129C21.2715 6.81402 22.2019 6.49403 22.3402 5.72323L22.9351 2.40656C23.0735 1.63454 22.3098 1.01089 21.5811 1.30086L18.6067 2.48435C17.917 2.7588 17.7561 3.66258 18.3089 4.1581Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <circle cx="16.8546" cy="8" r="1" fill="black"/>
+      <path d="M1.97484 13.775C2.05583 7.13715 6.7248 2.07528 12.1061 2.14094C17.4875 2.2066 22.0316 7.38089 21.9506 14.0187C21.9107 17.2875 20.7545 19.4979 19.0106 20.9042C17.237 22.3346 14.7332 23.0304 11.8517 22.9953C8.97014 22.9601 6.48409 22.2034 4.74588 20.7302C3.03689 19.2817 1.93495 17.0438 1.97484 13.775Z" stroke="black" stroke-width="2"/>
+      <path d="M17.3934 14.5C17.3934 15.3179 16.9114 16.1685 15.9077 16.8576C14.9086 17.5436 13.4733 18 11.8393 18C10.2054 18 8.77006 17.5436 7.77094 16.8576C6.76729 16.1685 6.28528 15.3179 6.28528 14.5C6.28528 13.6821 6.76729 12.8315 7.77094 12.1424C8.77006 11.4564 10.2054 11 11.8393 11C13.4733 11 14.9086 11.4564 15.9077 12.1424C16.9114 12.8315 17.3934 13.6821 17.3934 14.5Z" stroke="black" stroke-width="2"/>
+      </g>
+      <defs>
+      <clipPath id="clip0_12_1054">
+      <rect width="24" height="24" fill="white"/>
+      </clipPath>
+      </defs>
+      </svg>`,
+      lg: ``,
+      xl: ``,
+    },
+  },
+];
