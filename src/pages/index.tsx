997b910c0cd7b93a5b3d673e978046452f827755
manuelsanchez2 <manusansan22@gmail.com>
2023-05-19 14:44:37 +0200

    🆕 feat: Add two more icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home({ icons }: { icons: Icon[] }) {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#info-circle-lg-clip1)">
+                <g clipPath="url(#info-circle-lg-clip1)">
                   <path
                     d="M12.9951 7.1915C12.9951 7.74107 12.5496 8.18658 12 8.18658C11.4504 8.18658 11.0049 7.74107 11.0049 7.1915C11.0049 6.64193 11.4504 6.19641 12 6.19641C12.5496 6.19641 12.9951 6.64193 12.9951 7.1915Z"
                     fill="currentColor"
@@ -60,8 +60,8 @@ export default function Home({ icons }: { icons: Icon[] }) {
                     fill="currentColor"
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M12 24.1866C18.6274 24.1866 24 18.814 24 12.1866C24 5.55917 18.6274 0.186584 12 0.186584C5.37258 0.186584 0 5.55917 0 12.1866C0 18.814 5.37258 24.1866 12 24.1866ZM12 22.1866C17.5228 22.1866 22 17.7094 22 12.1866C22 6.66374 17.5228 2.18658 12 2.18658C6.47715 2.18658 2 6.66374 2 12.1866C2 17.7094 6.47715 22.1866 12 22.1866Z"
                     fill="currentColor"
                   />
@@ -85,7 +85,7 @@ export default function Home({ icons }: { icons: Icon[] }) {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#alert-square-lg-clip1)">
+                <g clipPath="url(#alert-square-lg-clip1)">
                   <path
                     d="M11.5 17.1915C11.5 16.6419 11.9455 16.1964 12.4951 16.1964C13.0447 16.1964 13.4902 16.6419 13.4902 17.1915C13.4902 17.7411 13.0447 18.1866 12.4951 18.1866C11.9455 18.1866 11.5 17.7411 11.5 17.1915Z"
                     fill="currentColor"
@@ -95,8 +95,8 @@ export default function Home({ icons }: { icons: Icon[] }) {
                     fill="currentColor"
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M0.5 3.37177C0.5 1.61264 1.92605 0.186584 3.68518 0.186584H21.3148C23.0739 0.186584 24.5 1.61264 24.5 3.37177V21.0014C24.5 22.7605 23.0739 24.1866 21.3148 24.1866H3.68518C1.92605 24.1866 0.5 22.7605 0.5 21.0014V3.37177ZM3.68518 2.18658C3.03062 2.18658 2.5 2.71721 2.5 3.37177V21.0014C2.5 21.656 3.03062 22.1866 3.68518 22.1866H21.3148C21.9694 22.1866 22.5 21.656 22.5 21.0014V3.37177C22.5 2.71721 21.9694 2.18658 21.3148 2.18658H3.68518Z"
                     fill="currentColor"
                   />
@@ -129,8 +129,8 @@ export default function Home({ icons }: { icons: Icon[] }) {
                   fill="currentColor"
                 />
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M9.3713 3.89092C10.5111 1.81859 13.4888 1.81858 14.6286 3.89092L22.246 17.7408C23.3457 19.7402 21.8992 22.1866 19.6174 22.1866H4.38254C2.10074 22.1866 0.654244 19.7402 1.75389 17.7408L9.3713 3.89092ZM12.8762 4.85475C12.4962 4.16398 11.5037 4.16397 11.1237 4.85475L3.50632 18.7047C3.13978 19.3711 3.62194 20.1866 4.38254 20.1866H19.6174C20.378 20.1866 20.8601 19.3711 20.4936 18.7047L12.8762 4.85475Z"
                   fill="currentColor"
                 />
@@ -143,7 +143,7 @@ export default function Home({ icons }: { icons: Icon[] }) {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#info-circle-lg-clip1)">
+                <g clipPath="url(#info-circle-lg-clip1)">
                   <path
                     d="M12.9951 7.1915C12.9951 7.74107 12.5496 8.18658 12 8.18658C11.4504 8.18658 11.0049 7.74107 11.0049 7.1915C11.0049 6.64193 11.4504 6.19641 12 6.19641C12.5496 6.19641 12.9951 6.64193 12.9951 7.1915Z"
                     fill="currentColor"
@@ -153,8 +153,8 @@ export default function Home({ icons }: { icons: Icon[] }) {
                     fill="currentColor"
                   />
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M12 24.1866C18.6274 24.1866 24 18.814 24 12.1866C24 5.55917 18.6274 0.186584 12 0.186584C5.37258 0.186584 0 5.55917 0 12.1866C0 18.814 5.37258 24.1866 12 24.1866ZM12 22.1866C17.5228 22.1866 22 17.7094 22 12.1866C22 6.66374 17.5228 2.18658 12 2.18658C6.47715 2.18658 2 6.66374 2 12.1866C2 17.7094 6.47715 22.1866 12 22.1866Z"
                     fill="currentColor"
                   />
