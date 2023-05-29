dfc7dd5929ac0b87c99c6a71314d3785dc21db3c
manuelsanchez2 <manusansan22@gmail.com>
2023-05-29 12:03:11 +0200

    🔥 hotfix: Fix

--- src/api/getIconsData.ts
+++ src/api/getIconsData.ts
@@ -3,8 +3,8 @@ import { Icon } from '@/types/types'
 export async function getIconsData(): Promise<Icon[]> {
   const url = `${process.env.APP_URL}/api/icons`
 
-  const res = await fetch(url)
-  const data = await res.json()
+  // const res = await fetch(url)
+  // const data = await res.json()
   // console.log(data)
 
   return []
