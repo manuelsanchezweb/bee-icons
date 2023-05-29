82dc27babaae85db4b964310d6498de6da178ad2
manuelsanchez2 <manusansan22@gmail.com>
2023-05-29 12:05:25 +0200

    🐛 fix: Fix the pipeline finally

--- src/api/getIconsData.ts
+++ src/api/getIconsData.ts
@@ -3,9 +3,8 @@ import { Icon } from '@/types/types'
 export async function getIconsData(): Promise<Icon[]> {
   const url = `${process.env.APP_URL}/api/icons`
 
-  // const res = await fetch(url)
-  // const data = await res.json()
-  // console.log(data)
+  const res = await fetch(url)
+  const data = await res.json()
 
-  return []
+  return data as Icon[]
 }
